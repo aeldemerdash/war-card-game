1ed14492f0654c89346d60a85831b39fc09d5d9e
R. Paul Kennedy <rpaulkennedy@gmail.com>
2017-12-16 15:08:39 -0800

    war-card-game

--- src/main/java/org/rpk/game/war/card/Dealer.java
+++ src/main/java/org/rpk/game/war/card/Dealer.java
@@ -3,7 +3,7 @@ package org.rpk.game.war.card;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Queue;
-import java.util.stream.IntStream;
+import static java.util.stream.IntStream.range;
 
 import org.rpk.game.war.Player;
 
@@ -28,12 +28,10 @@ public class Dealer {
 	 * all cards are removed from the deck.
 	 */
 	public void deal(Player... players) {
-		Queue<Player> playerQueue = new ArrayDeque<>(Arrays.asList(players));
-		IntStream.range(0, deck.getCards().size()).forEach(i -> {
-			Player player = playerQueue.remove();
-			player.getHand().add(deck.getCards().remove(0));
-			playerQueue.add(player);			
-		});
+		range(0, deck.getCards().size())
+			.forEach(i -> players[i % players.length]
+				.getHand()
+				.add(deck.getCards().remove(0)));
 	}
 	
 }
